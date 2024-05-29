efb97bf4debb12f15896e44ce449eb9cc6ec56cc
abhay3690 <abhay7777@gmail.com>
2024-05-29 23:04:56 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,11 @@
+const Student = {
+    fullname : "Abhay suthar",
+    age : 28,
+    cgpa : 745,
+    isPass : true
+};  
+console.log(Student);
+// for(let i =1; i>=0; i++){
+//     console.log("i = ", i);
+// }
+console.log("student age is : "+ Student.age);
