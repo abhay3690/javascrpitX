3c21bc1dec543bb3bcd2e0978756d701f0a4821e
abhay3690 <abhay7777@gmail.com>
2024-05-30 16:34:51 +0530

    add

--- first.js
+++ first.js
@@ -1,11 +1,68 @@
-const Student = {
-    fullname : "Abhay suthar",
-    age : 28,
-    cgpa : 745,
-    isPass : true
-};  
-console.log(Student);
-// for(let i =1; i>=0; i++){
-//     console.log("i = ", i);
+// const Student = {
+//     fullname : "Abhay suthar",
+//     age : 28,
+//     cgpa : 745,
+//     isPass : true
+// };  
+// console.log(Student);
+// // for(let i =1; i>=0; i++){
+// //     console.log("i = ", i);
+// // }
+// console.log("student age is : "+ Student.age);
+
+
+
+
+let charecter = ["Wow","variable","Constanat","Integer","Float"];
+for(let id =0; id<charecter.length; id++){
+    console.log(charecter[id]);
+}
+
+
+
+// Prompt the user to enter a string
+// var userInput = prompt("Please enter a string:");
+
+// // Prompt the user to enter a substring to check at the beginning
+// var startSubstring = prompt("Please enter a substring to check at the beginning:");
+
+// // Prompt the user to enter a substring to check at the end
+// var endSubstring = prompt("Please enter a substring to check at the end:");
+
+// // Check if the user input starts with the startSubstring
+// if (userInput.startsWith(startSubstring)) {
+//     alert("The string starts with '" + startSubstring + "'");
+// } else {
+//     alert("The string does not start with '" + startSubstring + "'");
 // }
-console.log("student age is : "+ Student.age);
+
+// // Check if the user input ends with the endSubstring
+// if (userInput.endsWith(endSubstring)) {
+//     alert("The string ends with '" + endSubstring + "'");
+// } else {
+//     alert("The string does not end with '" + endSubstring + "'");
+// }
+
+
+
+// Example string
+// var str = "Hello, world!";
+// if (str.startsWith("Hello")) {
+//     console.log("The string starts with 'Hello'");
+// } else {
+//     console.log("The string does not start with 'Hello'");
+// }
+// if (str.endsWith("world!")) {
+//     console.log("The string ends with 'world!'");
+// } else {
+//     console.log("The string does not end with 'world!'");
+// }
+
+
+// var age = prompt("Please enter your age:");
+// age = parseInt(age);
+// if (age >= 18) {
+//     alert("You are old enough to vote!");
+// } else {
+//     alert("You are not old enough to vote.");
+// }
